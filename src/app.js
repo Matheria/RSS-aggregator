7ff46f18b2840764126766e5ee566e721278db9d
Matheria <sensenevercame@gmail.com>
2022-10-15 17:33:15 +0500

    feat: i18n

--- src/app.js
+++ src/app.js
@@ -1,10 +1,35 @@
 //import axios from "axios";
+import i18next from "i18next";
 import onChange from "on-change";
 import _ from "lodash";
+import * as yup from "yup";
 import validate from "./validate.js";
 import render from "./view.js";
+import ru from "./locales/ru";
 
 export default () => {
+  const defaultLanguage = "ru";
+
+  const i18nextInstance = i18next.createInstance();
+  i18nextInstance
+    .init({
+      lng: defaultLanguage,
+      debug: true,
+      resources: {
+        ru,
+      },
+    })
+    .then(() => {
+      yup.setLocale({
+        string: {
+          url: "invalidURL",
+        },
+        mixed: {
+          notOneOf: "rssExist",
+        },
+      });
+    });
+
   const state = onChange(
     {
       form: {
---
+++ src/locales/ru.js
@@ -0,0 +1,18 @@
+export default {
+  translation: {
+    feeds: "Фиды",
+    posts: "Посты",
+    buttoms: {
+      view: "Просмотр",
+      close: "Закрыть",
+      readArticle: "Читать полностью",
+    },
+    success: "RSS успешно загружен",
+    errors: {
+      invalidURL: "Ссылка должна быть валидным URL",
+      rssAlreadyExist: "RSS уже существует",
+      parseError: "Ресурс не содержит валидный RSS",
+      networkError: "Ошибка сети",
+    },
+  },
+};
