baaf8b2b85aeb28e84a72e44ffe4925991e8aa34
Matheria <sensenevercame@gmail.com>
2022-10-17 17:11:04 +0500

    refactor: delete validate.js

--- src/validate.js
+++ src/validate.js
@@ -1,8 +1,1 @@
-import * as yup from "yup";
 
-export default (link, feeds) => {
-  const allFeeds = feeds.map(({ feed }) => feed);
-  const schema = yup.string().url().notOneOf(allFeeds);
-
-  return schema.validate(link);
-};
