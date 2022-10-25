294c96a77f2e3b1c87b8e6f12276b059971eeda8
Matheria <sensenevercame@gmail.com>
2022-10-25 12:51:25 +0500

    feat: add normalize data function

---
+++ src/normalizeData.js
@@ -0,0 +1,23 @@
+export default (data) => {
+  const title = data.querySelector("title").textContent;
+  const description = data.querySelector("description").textContent;
+
+  const feed = {
+    title,
+    description,
+  };
+
+  const posts = [...data.querySelectorAll("item")].map((item) => {
+    const postTitle = item.querySelector("title").textContent;
+    const postDescription = item.querySelector("description").textContent;
+    const postLink = data.querySelector("link").textContent;
+
+    return {
+      title: postTitle,
+      description: postDescription,
+      link: postLink,
+    };
+  });
+
+  return { feed, posts };
+};
